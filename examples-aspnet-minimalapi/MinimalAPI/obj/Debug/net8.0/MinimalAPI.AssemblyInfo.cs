de1b9dacc9b30d33dcdce3a95a38f677d3b2a8c8
novaritzz <viktor.carpels@student.howest.be>
2024-11-21 16:36:12 +0100

    "Test staging workflow"

--- examples-aspnet-minimalapi/MinimalAPI/obj/Debug/net8.0/MinimalAPI.AssemblyInfo.cs
+++ examples-aspnet-minimalapi/MinimalAPI/obj/Debug/net8.0/MinimalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22bf90354202b509840394f72a35fae968cc48b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
